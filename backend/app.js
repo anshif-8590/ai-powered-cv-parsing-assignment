04f0844487c3e050bf66d41559d9b7335d159167
anshif-8590 <anshifp949@gmail.com>
2025-12-15 15:24:30 +0530

    Add backend and frontend as normal tracked folders

---
+++ backend/app.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import connectDB from './src/config/db.js';
+import dotenv from 'dotenv';
+import cors from 'cors'
+import Route from './src/routes/index.js';
+
+
+dotenv.config();
+
+const app = express();
+const Name = 'CV parsing web app '
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(cors())
+connectDB()
+
+
+
+
+app.use('/api/v1', Route);
+
+app.listen(PORT, () => {
+  console.log(`Server started successfully at http://localhost:${PORT} - ${Name} backend service!`); 
+});
---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import { Routes, Route } from "react-router";
+
+function App() {
+  return (
+   <>
+    <Routes>
+    <Route>
+      
+    </Route>
+    </Routes>
+   </>
+  );
+}
+
+export default App;
